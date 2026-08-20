ddd912932c4afdad224ebe8790c8e647c4bcb8ef
dalekopping <dalekopping@gmail.com>
2026-08-20 13:02:22 +0200

    Bump release for booking colour cache refresh

--- wp-content/themes/dk-expressions-v4-approved-fixes/functions.php
+++ wp-content/themes/dk-expressions-v4-approved-fixes/functions.php
@@ -99,7 +99,7 @@ function dkxv4_is_conversion_landing_preview() {
 }
 
 function dkx_fixes_assets() {
-	$release = '1.22.1';
+	$release = '1.22.2';
 
 	wp_enqueue_style( 'dkx-parent-style', get_template_directory_uri() . '/style.css', array(), '1.0.0' );
 	wp_enqueue_style( 'dkx-approved-fixes', get_stylesheet_uri(), array( 'dkx-parent-style' ), $release );
